26be8c6c114669efe5ea70429aaca0835e7fb7ba
ckay <144651152+ckomen@users.noreply.github.com>
2025-11-03 21:13:17 +0300

    Add solution - CodeHub

---
+++ 5-kyu/the-hashtag-generator/the-hashtag-generator.py
@@ -0,0 +1,11 @@
+def generate_hashtag(s):
+    words = s.strip().split()
+    if not words:
+        return False
+    hashtag = '#' + ''.join(word.capitalize() for word in words)
+    if len(hashtag) > 140:
+        return False
+    
+    return hashtag
+​
+​
