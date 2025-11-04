2c6fc034ef90613b071def3a3d668af89479bd35
ckay <144651152+ckomen@users.noreply.github.com>
2025-11-04 20:54:58 +0300

    Add solution - CodeHub

---
+++ 5-kyu/moving-zeros-to-the-end/moving-zeros-to-the-end.py
@@ -0,0 +1,12 @@
+def move_zeros(arr):
+    pos = 0  
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[pos] = arr[i]
+            pos += 1
+    while pos < len(arr):
+        arr[pos] = 0
+        pos += 1
+    
+    return arr
+​
