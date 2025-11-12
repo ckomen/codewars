2c6570c1411ab91916b7c112c7c9feab44f7a02a
ckay <144651152+ckomen@users.noreply.github.com>
2025-11-12 21:08:49 +0300

    Add solution - CodeHub

---
+++ 4-kyu/a-simplistic-tcp-finite-state-machine-fsm/a-simplistic-tcp-finite-state-machine-fsm.py
@@ -0,0 +1,28 @@
+def traverse_TCP_states(events):
+    transitions = {
+        "CLOSED": {
+            "APP_PASSIVE_OPEN": "LISTEN",
+            "APP_ACTIVE_OPEN": "SYN_SENT",
+        },
+        "LISTEN": {
+            "RCV_SYN": "SYN_RCVD",
+            "APP_SEND": "SYN_SENT",
+            "APP_CLOSE": "CLOSED",
+        },
+        "SYN_RCVD": {
+            "APP_CLOSE": "FIN_WAIT_1",
+            "RCV_ACK": "ESTABLISHED",
+        },
+        "SYN_SENT": {
+            "RCV_SYN": "SYN_RCVD",
+            "RCV_SYN_ACK": "ESTABLISHED",
+            "APP_CLOSE": "CLOSED",
+        },
+        "ESTABLISHED": {
+            "APP_CLOSE": "FIN_WAIT_1",
+            "RCV_FIN": "CLOSE_WAIT",
+        },
+        "FIN_WAIT_1": {
+            "RCV_FIN": "CLOSING",
+            "RCV_FIN_ACK": "TIME_WAIT",
+            "RCV_ACK": "FIN_WAIT_2",
