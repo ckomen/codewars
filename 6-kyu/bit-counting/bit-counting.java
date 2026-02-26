9428a349006590c8aad1c38ca9e8800bbf7ae7cf
ckay <144651152+ckomen@users.noreply.github.com>
2026-02-26 03:50:03 +0300

    Add solution - CodeHub

---
+++ 6-kyu/bit-counting/bit-counting.java
@@ -0,0 +1,13 @@
+public class BitCounting {
+​
+    public static int countBits(int n){
+        int count = 0;
+​
+        while (n > 0) {
+            n = n & (n - 1);  // removes the lowest set bit
+            count++;
+        }
+​
+        return count;
+    }
+}
